66a9b98a9dc9a10b6bd15ffea0d3b4447b3fdda3
Rohith <rohithvaidya@gmail.com>
2023-03-24 19:58:51 +0530

    Updated DPLL solver for agent

---
+++ agent.py
@@ -0,0 +1,147 @@
+from dpll_solver import *
+
+
+
+def check_all_literals():
+    all_lit = []
+    for i in KB:
+        for j in i:
+            all_lit.append(j[0])      
+    for i in all_lit:
+        if i not in assignments.keys():
+            return 0
+    return 1
+
+KB = []
+
+p = open("percepts.txt", "r")
+percepts = p.readlines()
+
+start_pos = 0
+
+env = [[0, "b", "P", "b"],
+        ["s", 5, "b", 7],
+        ["W", "b,s", "P", "b"],
+        ["s", 13, "b", "G"]]
+
+
+
+class box:
+    def __init__(self):
+        self.id = 0
+        self. status = 0
+        self.percept = []
+
+#Building the grid
+l=[]
+ctr = 0
+grid= []
+for i in range(4):
+    for j in range(4):
+        ob = box()
+        ob.id = ctr
+        if((str(env[i][j]).isalpha()) and ("," not in env[i][j])):
+            ob.percept.append(env[i][j])
+        elif("," in str(env[i][j])):
+            ob.percept.extend(env[i][j].split(",")) 
+        else:
+            pass
+        l.append(ob)
+        ctr+=1
+    grid.append(l)
+    l=[]
+
+#Building Adjancency List
+graph = {}
+l = []
+for i in range(4):
+    for j in range(4):
+        if(j+1 < 4):
+            l.append(grid[i][j+1])
+            graph[grid[i][j]] = l
+        if(i+1 < 4):
+            l.append(grid[i+1][j])
+            graph[grid[i][j]] = l
+        if(i-1>=0):
+            l.append(grid[i-1][j])
+            graph[grid[i][j]] = l
+        if(j-1>=0):
+            l.append(grid[i][j-1])
+            graph[grid[i][j]] = l
+        l = []
+
+#Intial Conditions for KB - Literals are P, W
+KB.append({("P0", False)})
+KB.append({("W0", False)})
+#KB.append({("W1", False)})
+#KB.append({("P1", False)})
+#KB.append({("W4", False)})
+#KB.append({("P4", False)})
+
+
+visited = set() 
+stack = [grid[0][0]]
+#visited.add(0)
+
+sat = None
+assignments = None
+
+while(stack!=[]):
+    top = stack.pop()
+
+    if(top.id not in visited):
+        #Visiting the Cell
+        print(top.id)
+        top.status = 1
+        formula = set()
+        #Getting Percept
+        print("Percept", top.percept)
+        if("s" not in top.percept):
+            for i in graph[top]:
+                if(i.id not in visited):
+                    formula.add(("W{}".format(i.id), False))
+                    KB.append(formula)
+                    formula = set()
+
+        if("b" not in top.percept):
+            for i in graph[top]:
+                if(i.id not in visited):
+                    formula.add(("P{}".format(i.id), False))
+                    KB.append(formula)
+                    formula = set()
+
+        if("s" in top.percept):
+            for i in graph[top]:
+                
+                if(i.id not in visited):
+                    formula.add(("W{}".format(i.id), True))
+                    print(formula)
+            KB.append(formula)
+            formula = set()
+
+        if("b" in top.percept):
+            for i in graph[top]:
+                if(i.id not in visited):
+                    formula.add(("P{}".format(i.id), True))
+            KB.append(formula)
+            formula = set()
+        
+        for i in graph[top]:
+            sat, assignments = dpll(KB)
+            #print(KB)
+            #print(assignments)
+            if(check_all_literals()):
+                stack.append(i)
+            else:
+                #Back Track
+                for p in graph.keys():
+                    if((top in graph[p]) and (p in visited)):
+                        stack.append(p)
+                        visited.remove(p.id)
+                #stack.append(visited[-1])           
+    else:
+        pass
+
+    
+    visited.add(top.id)
+
---
+++ dpll_solver.py
@@ -0,0 +1,34 @@
+# The Formula is of the form (a V b V c)
+#The CNF formula is thus a list of disjunctive clauses . aka our KB
+
+#KB = [{("p", True)}, {("p", False)}]
+
+def __select_literal(cnf):
+    for c in cnf:
+        for literal in c:
+            return literal[0]
+ 
+def dpll(cnf, assignments={}):
+ 
+    if len(cnf) == 0:
+        return True, assignments
+ 
+    if any([len(c)==0 for c in cnf]):
+        return False, None
+ 
+    l = __select_literal(cnf)
+ 
+    new_cnf = [c for c in cnf if (l, True) not in c]
+    new_cnf = [c.difference({(l, False)}) for c in new_cnf]
+    sat, vals = dpll(new_cnf, {**assignments, **{l: True}})
+    if sat:
+        return sat, vals
+ 
+    new_cnf = [c for c in cnf if (l, False) not in c]
+    new_cnf = [c.difference({(l, True)}) for c in new_cnf]
+    sat, vals = dpll(new_cnf, {**assignments, **{l: False}})
+    if sat:
+        return sat, vals
+ 
+    return False, None
+
